0463481f1025184435f5aaff97b1d92e861ee86d
SorayaSantos <44404904+SorayaSantos@users.noreply.github.com>
2018-12-29 22:52:40 +0000

    table with scroll and click
    table with scroll and click

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -8,10 +8,10 @@ import { LandingComponent } from './landing/landing.component';
 import { MoviesComponent } from './movies/movies.component';
 import { UsersComponent } from './users/users.component';
 import { HeaderComponent } from '../shared/components/index';
-import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule,MatInputModule } from '@angular/material';
 import { TablesComponent } from './shared/components/tables/tables.component';
+import { FormsModule } from '@angular/forms';
 
-import {NgxPaginationModule} from 'ngx-pagination'; 
 
 @NgModule({
   declarations: [
@@ -27,9 +27,11 @@ import {NgxPaginationModule} from 'ngx-pagination';
     CommonModule, 
     MatTableModule,
     MatPaginatorModule,
+    MatFormFieldModule,
     MatSortModule,
+    MatInputModule,
+    FormsModule,
     LayoutRoutingModule,
-    NgxPaginationModule
   ]
 })
 export class LayoutModule { }
--- src/app/layout/shared/components/tables/tables.component.ts
+++ src/app/layout/shared/components/tables/tables.component.ts
@@ -14,5 +14,7 @@ export class TablesComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  getMovieDetails(row){
+    console.log("click working");
+  }
 }
