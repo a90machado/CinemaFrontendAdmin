72338b5006ee42c3a4fe39d9d7ff9f3ef5ea8e57
SorayaSantos <44404904+SorayaSantos@users.noreply.github.com>
2018-12-30 21:43:40 +0000

    details of movie appearing on row click working
    must put table more beatiful and include buttons to delete, update and create

--- src/app/layout/shared/components/tables/tables.component.ts
+++ src/app/layout/shared/components/tables/tables.component.ts
@@ -9,12 +9,37 @@ export class TablesComponent implements OnInit {
   @Input() header: any;
   @Input() data$: any;
   @Input() theme = 'table-dark'
+  @Input() title = ''
+  @Input() minimumAge = ''
+  @Input() duration = ''
+  @Input() durationMovie = ''
+  @Input() releaseDate = ''
+  @Input() releaseDateMovie = ''
+  @Input() director = ''
+  @Input() directorMovie = ''
+  @Input() cast = ''
+  @Input() castMovie = ''
+  @Input() synopsis = ''
+  @Input() synopsisMovie = ''
+  @Input() image = ''
 
   constructor() { }
 
   ngOnInit() {
   }
   getMovieDetails(row){
-    console.log(row.title);
+    this.title = row.title;
+    this.duration = "Duration: ";
+    this.durationMovie = row.duration + " min."
+    this.minimumAge= row.minimumAge;
+    this.releaseDate= "Release Date: ";
+    this.releaseDateMovie= row.releaseDate;
+    this.director= "Director: ";
+    this.directorMovie= row.director;
+    this.cast= "Cast: ";
+    this.castMovie= row.cast;
+    this.synopsis= "Synopsis: ";
+    this.synopsisMovie= row.synopsis;
+    this.image= row.image;
   }
 }
