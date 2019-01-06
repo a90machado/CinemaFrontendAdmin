b003013c4bfb96071639f9ddc581db07fbca0156
SorayaSantos <44404904+SorayaSantos@users.noreply.github.com>
2019-01-06 03:51:29 +0000

    finally edit is working
    Important: Code is only prepared to work in my pc, to work in others pcs it's necessary to change string releaseDate and EndDate - take off disnecessary zeros. Code to change dateformat is comented.
    
    Other thing - user can choose a date to end before date to release - its a problem - must resolve.
    
    Other thing - button for edit is ugly and doesn't close modal- need to change that.

--- src/app/layout/shared/components/modals/edit-movie-modal/edit-movie-modal.component.ts
+++ src/app/layout/shared/components/modals/edit-movie-modal/edit-movie-modal.component.ts
@@ -12,12 +12,109 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
 export class EditMovieModalComponent implements OnInit {
 
   movie: Movie = new Movie();
+  dayRelease = "";
+  monthRelease = "";
+  yearRelease = "";
+  dayEnd= "";
+  monthEnd = "";
+  yearEnd = "";
+
+
   constructor( private movieApiService: MovieApiService, private dataService: DataService, public modalRef: BsModalRef ) { }
 
   ngOnInit() {
   }
-  editMovie(){
-  this.movieApiService.editMovie(this.movie).subscribe(() =>{
-    this.dataService.updateMovies();
-  });}
+
+  editMovie(id,title,image,minimumAge,duration,releaseDate,endDate,director,cast,synopsis){
+    if (this.monthRelease=="January") {
+      this.monthRelease="01";
+    }
+    if (this.monthRelease=="February") {
+      this.monthRelease="02";
+    }
+    if (this.monthRelease=="March") {
+      this.monthRelease="03";
+    }
+    if (this.monthRelease=="April") {
+      this.monthRelease="04";
+    }
+    if (this.monthRelease=="May") {
+      this.monthRelease="05";
+    }
+    if (this.monthRelease=="June") {
+      this.monthRelease="06";
+    }
+    if (this.monthRelease=="July") {
+      this.monthRelease="07";
+    }
+    if (this.monthRelease=="August") {
+      this.monthRelease="08";
+    }
+    if (this.monthRelease=="September") {
+      this.monthRelease="09";
+    }
+    if (this.monthRelease=="October") {
+      this.monthRelease="10";
+    }
+    if (this.monthRelease=="November") {
+      this.monthRelease="11";
+    }
+    if (this.monthRelease=="December") {
+      this.monthRelease="12";
+    }
+    if (this.monthEnd=="January") {
+      this.monthEnd="01";
+    }
+    if (this.monthEnd=="February") {
+      this.monthEnd="02";
+    }
+    if (this.monthEnd=="March") {
+      this.monthEnd="03";
+    }
+    if (this.monthEnd=="April") {
+      this.monthEnd="04";
+    }
+    if (this.monthEnd=="May") {
+      this.monthEnd="05";
+    }
+    if (this.monthEnd=="June") {
+      this.monthEnd="06";
+    }
+    if (this.monthEnd=="July") {
+      this.monthEnd="07";
+    }
+    if (this.monthEnd=="August") {
+      this.monthEnd="08";
+    }
+    if (this.monthEnd=="September") {
+      this.monthEnd="09";
+    }
+    if (this.monthEnd=="October") {
+      this.monthEnd="10";
+    }
+    if (this.monthEnd=="November") {
+      this.monthEnd="11";
+    }
+    if (this.monthEnd=="December") {
+      this.monthEnd="12";
+    }
+    releaseDate=this.yearRelease+"-"+this.monthRelease+"-"+this.dayRelease+"T00:00:00";
+    endDate=this.yearEnd+"-"+this.monthEnd+"-"+this.dayEnd+"T00:00:00";
+
+    console.log(id,title,image,minimumAge,duration,director,cast,synopsis,releaseDate,endDate);
+    this.movie.id=id;
+    this.movie.title=title;
+    this.movie.image=image;
+    this.movie.minimumAge=minimumAge;
+    this.movie.duration=duration;
+    this.movie.director=director;
+    this.movie.cast=cast;
+    this.movie.synopsis=synopsis;
+    this.movie.releaseDate=releaseDate;
+    this.movie.endDate=endDate;
+
+    this.movieApiService.editMovie(this.movie).subscribe(() =>{
+      this.dataService.updateMovies();
+    });
+  }
 }
