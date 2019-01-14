f51607135717f8010a377a78e92ca4a8f004a8cf
aluno manha <alunomanha@altars-Mac-mini-8.local>
2019-01-14 18:38:10 +0000

    n sei

--- src/app/layout/shared/components/modals/edit-user-modal/edit-user-modal.component.ts
+++ src/app/layout/shared/components/modals/edit-user-modal/edit-user-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 import { AccountApi, Account } from 'src/app/shared/sdk';
 import { DataService } from '../../../services';
 
@@ -12,19 +12,29 @@ export class EditUserModalComponent implements OnInit {
 
   account: Account = new Account();
   message = '';
+  roles = ['admin', 'user'];
 
   @Input()id: number;
+  @Input()name = '';
+  @Input()role = '';
+  @Input()email = '';
+  @Input()username= '';
 
   constructor(  public _modalRef: BsModalRef,
-
                 private _accountApi: AccountApi,
-                private _dataService: DataService) { }
+                private _dataService: DataService,
+              ) { }
 
   ngOnInit() {
 
   }
 
+  resetPassword(){
+    this._accountApi.res
+  }
+
   updateUser() {
+    this.account.role = this.role;
     this._accountApi.updateAttributes(this.id, this.account).subscribe(() => {
       this._modalRef.hide();
       this._dataService.updateAccounts();
