9e7c4f2b1b5e92408a12cca38dc455561c92e221
SorayaSantos <soraya.ss_3@hotmail.com>
2019-01-16 15:46:37 +0000

    edit room working

--- src/app/layout/shared/components/modals/edit-room-modal/edit-room-modal.component.ts
+++ src/app/layout/shared/components/modals/edit-room-modal/edit-room-modal.component.ts
@@ -21,6 +21,12 @@ export class EditRoomModalComponent implements OnInit {
   numberOfSeatsPerQueueOld:any;
   movieOld:any;
   cinemaObject:any;
+  newBreakString: String;
+  newHourString: String;
+  newMinutesString: String;
+  timeOpenNumber;
+  timeCloseNumber;
+  pauseNumber;
 
   public movies$: ReplaySubject<any>;
   public cinemas$: ReplaySubject<any>;
@@ -49,7 +55,7 @@ export class EditRoomModalComponent implements OnInit {
     this.cinemas$.subscribe((a) => {
       for (let i = 0; i < a.length; i++) {
         if (a[i].name==this.cinema) {
-          this.cinemaObject = a[i]
+          this.cinemaObject = JSON.parse(JSON.stringify(a[i]));
         }
       }
     });
@@ -59,16 +65,60 @@ export class EditRoomModalComponent implements OnInit {
     this.room.movie=this.movieObject;
     this.room.numberOfQueues=this.numberOfQueues;
     this.room.numberOfSeatsPerQueue=this.numberOfSeatsPerQueue;
+  
+    this.timeOpenNumber=(Number(this.getHour(this.room.cinema.timeOpen))*60)+(Number(this.getMinutes(this.room.cinema.timeOpen)));
+    this.timeCloseNumber=(Number(this.getHour(this.room.cinema.timeClose))*60)+(Number(this.getMinutes(this.room.cinema.timeClose)));
+    this.pauseNumber=Number(this.getBreak(this.room.cinema.pause));
+    this.room.cinema.timeOpen=this.timeOpenNumber;
+    this.room.cinema.timeClose=this.timeCloseNumber;
+    this.room.cinema.pause=this.pauseNumber;
 
     if (this.movieOld!=this.room.movie.title||this.numberOfQueuesOld!=this.room.numberOfQueues||this.numberOfSeatsPerQueueOld!=this.numberOfSeatsPerQueue) {
       this.roomService.editRoom(this.room).subscribe(()=>{
         this.dataService.updateRooms();
         this.modalRef.hide();
       })
-      console.log(this.room);
+      console.log(this.room)
       console.log("fez pedido")
     }
 
+  }
+  getBreak(breakstring){
+    this.newBreakString="";
+    for (let i = 0; i < breakstring.length; i++) {
+      if (breakstring[i]!=" ") {
+        this.newBreakString+=breakstring[i];
+      }
+      else{
+        break;
+      }
+    }
+    return this.newBreakString;
+  }
+
+  getHour(stringHour){
+    this.newHourString="";
+    for (let i = 0; i < stringHour.length; i++) {
+      if (stringHour[i]!="H") {
+        this.newHourString+=stringHour[i];
+      }
+      else{
+        break;
+      }
+    }
+    return this.newHourString;
+  }
+  getMinutes(stringHour){
+    this.newMinutesString="";
+    for (let i = 0; i < stringHour.length; i++) {
+      if (stringHour[i]==":") {
+        this.newMinutesString=stringHour[i+1]+stringHour[i+2];
+      }
+      else{
+        continue;
+      }
+    }
+    return this.newMinutesString;
   }
 
 }
